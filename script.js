4ed814cfca5bacc55c9a37d7866fa4cb7c70923c
nurmatullayevrasuljon <148245555+nurmatullayevrasuljon@users.noreply.github.com>
2026-08-14 09:26:03 +0300

    url lar o'zgartilidi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://mark1-crm-sytem.onrender.com/api";
+const API_URL = "https://mark1-crm-sytem.onrender.com";
 // ============================================================
 // 📦 USER DATA LOADING (SODDALASHTIRILGAN)
 // ============================================================
